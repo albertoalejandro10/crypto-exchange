bccf23bde5e99085812bb6d9b9c4036e4507ef9b
AlbertoAlejandro <alberto196g@gmail.com>
2024-07-25 16:58:48 -0400

    Added creator and github profiles

---
+++ src/components/GitHubButtonComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <a href="https://github.com/albertoalejandro10/" target="_blank">
+    <button
+      class="inline-flex items-center justify-center rounded-lg border border-gray-200 bg-white px-2 py-2 text-gray-500 transition hover:text-gray-700 focus:outline-none focus:ring"
+    >
+      <span class="text-3xl icon-[mdi--github]"></span>
+    </button>
+  </a>
+</template>
