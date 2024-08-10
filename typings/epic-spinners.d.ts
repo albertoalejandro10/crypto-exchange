6c7132df73f68400c4fe49556fac2ab179511572
AlbertoAlejandro <alberto196g@gmail.com>
2024-08-09 22:26:28 -0400

    Updated Typescript settings

--- typings/epic-spinners.d.ts
+++ typings/epic-spinners.d.ts
@@ -1,2 +1,14 @@
 // typings/epic-spinners.d.ts
-declare module 'epic-spinners'
+declare module 'epic-spinners' {
+  import { Component } from 'vue'
+
+  export interface SpinnerProps {
+    animationDuration?: number
+    size?: number
+    color?: string
+  }
+
+  export class FlowerSpinner extends Component {
+    constructor(props: SpinnerProps)
+  }
+}
